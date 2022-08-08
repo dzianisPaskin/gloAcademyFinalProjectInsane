c5462bd0d366801cb8e5040019f21386299dd602
Dzianis <paskin.denis.00gmail.com>
2022-08-08 20:35:15 +0200

    fixed the service list

--- src/modules/openServicesList.js
+++ src/modules/openServicesList.js
@@ -1,7 +1,8 @@
 export const openServicesList = () => {
   const modal = document.querySelector(".popup-repair-types");
   const link = document.querySelector(".link-list .menu-link");
-  const link2 = document.querySelector(".link-list a");
+  const link1 = document.querySelector(".link-services-1");
+  const link2 = document.querySelector(".link-services-2");
   const menu = document.querySelector(".popup-dialog-menu");
 
   const showModal = () => {
@@ -9,10 +10,25 @@ export const openServicesList = () => {
     menu.style.transform = `translate3d(645px, 0, 0)`;
   }
   link.addEventListener("click", showModal);
-  link2.addEventListener("click",showModal);
+  link1.addEventListener("click", () => {
+    console.log('link')
+    modal.style.visibility = "visible";
+    menu.style.transform = `translate3d(645px, 0, 0)`;
+  });
+  link2.addEventListener("click", () => {
+    console.log('link')
+    modal.style.visibility = "visible";
+    menu.style.transform = `translate3d(645px, 0, 0)`;
+  });
 
   modal.addEventListener('click', (e) => {
-    if(e.target.closest('.mobile-hide')) {
+    if(e.target.closest('.mobile-hide') || !e.target.closest('.popup-dialog')) {
+      modal.style.visibility = "hidden";
+    }
+  })
+
+  window.addEventListener('keydown', (e) => {
+    if(e.keyCode === 27) {
       modal.style.visibility = "hidden";
     }
   })
