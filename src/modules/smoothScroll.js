9a0cfb922ba5e03c6c3a8ab87e9740967ea53c62
Dzianis <paskin.denis.00gmail.com>
2022-08-08 14:19:47 +0200

    added smooth scrolling

--- src/modules/smoothScroll.js
+++ src/modules/smoothScroll.js
@@ -2,6 +2,7 @@ export const smoothScroll = () => {
   const menuItems = document.querySelectorAll(".popup-menu-nav__item>a");
   const mainItem = document.querySelector(`a[href='#main']`);
   const btnFooter = document.querySelector(".button-footer");
+  const menu = document.querySelector(".popup-dialog-menu");
 
   const scroll = (e) => {
     e.preventDefault();
@@ -10,15 +11,18 @@ export const smoothScroll = () => {
       behavior: "smooth",
       block: "start",
     });
+
   };
 
-  
+
   mainItem.addEventListener("click", scroll);
   btnFooter.addEventListener("click", scroll);
 
   menuItems.forEach((anchor) => {
     anchor.addEventListener("click", function (e) {
       e.preventDefault();
+      let height = document.documentElement.clientWidth;
+      height < 576 ? menu.style.transform = `translate3d(0, -100vh, 0)` : menu.style.transform = `translate3d(555px, 0, 0)`;
 
       const blockID = anchor.getAttribute("href");
 
