71a92c8b9635e0e17c7221c634edc3c1e80c075e
Dzianis <paskin.denis.00gmail.com>
2022-08-08 19:59:00 +0200

    fixed the adaptive for the menu

--- src/modules/showMenu.js
+++ src/modules/showMenu.js
@@ -1,20 +1,35 @@
-
-
 export const showMenu = () => {
   const menuIcon = document.querySelector(".menu__icon");
   const menu = document.querySelector(".popup-dialog-menu");
   const closeMenu = document.querySelector(".close-menu");
-  let height = document.documentElement.clientWidth;
+
 
   menuIcon.addEventListener("click", () => {
     menu.style.transform = `translate3d(0, 0, 0)`;
   });
-
   closeMenu.addEventListener("click", () => {
-    if (height < 576) {
-      menu.style.transform = `translate3d(0, -100vh, 0)`;
-    } else {
+    menu.style.transform = `translate3d(645px, 0, 0)`;
+  });
+
+
+  window.addEventListener("resize", () => {
+    let width = document.documentElement.clientWidth;
+    if (width < 576) {
+      menu.style.transform = `translate3d(0, -150vh, 0)`;
+      menuIcon.addEventListener("click", () => {
+        menu.style.transform = `translate3d(0, 0, 0)`;
+      });
+      closeMenu.addEventListener("click", () => {
+        menu.style.transform = `translate3d(0, -150vh, 0)`;
+      });
+    } else if (width > 576) {
       menu.style.transform = `translate3d(645px, 0, 0)`;
+      menuIcon.addEventListener("click", () => {
+        menu.style.transform = `translate3d(0, 0, 0)`;
+      });
+      closeMenu.addEventListener("click", () => {
+        menu.style.transform = `translate3d(645px, 0, 0)`;
+      });
     }
   });
 };
